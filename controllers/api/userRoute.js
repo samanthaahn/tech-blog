2fa64c59276da8041f484eedba8b3e3b9340ffe4
Samantha Ahn <samanthaahn@Samanthas-MBP.attlocal.net>
2023-04-26 16:51:47 -0700

    completed the user route git push '

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -42,8 +42,14 @@ try {
 });
 
 //logout
-router.post('/', async (req, res) => {
-
+router.post('/logout', (req, res) => {
+if (req.session.loggedIn) {
+    req.session.destroy(() => {
+res.status(204).end();
+    });
+}else {
+    res.status(404).end();
+}
 });
 
 module.exports = router;
