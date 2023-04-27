50223dd810ec3d32b40f9b728a0272927e24bbcc
Samantha Ahn <samanthaahn@Samanthas-MBP.attlocal.net>
2023-04-27 15:27:37 -0700

    finished post and comment route

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -3,7 +3,19 @@ const { Comment } = require('../../models');
 
 
 router.get('/', async (req, res) => {
-res.send("This is a comment!");
+try {
+    const {post_id, content, user_id} = req.body;
+
+    const newComment = await Comment.create({
+        post_id,
+        content,
+        user_id: req.session.userId
+    });
+    res.status(200).json(newComment);
+} catch (error) {
+    console.error('Error with this post:', err);
+    res.status(500).json({ message: 'Internal Server Error', error: err });
+}
 });
 
 module.exports = router;
--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 
+//Get all posts
 router.get('/', async (req, res) => {
-res.send("This is a blog post!");
+try {
+    const postData = await Post.findAll({
+        include: [{ model:User}, {model: Comment}],
+    });
+    res.status(200).json(postData);
+} catch (error) {
+    res.status(500).json(error);
+}
 });
+
+//Get one post
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.find(req.params.id, {
+            include: [{ model: User}, {model: Comment}],
+        });
+    if (!postData) {
+        res.status(404).json({message: 'No post found! Try again.'});
+        return;
+    }
+    res.status(200).json(postData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 
 module.exports = router;
