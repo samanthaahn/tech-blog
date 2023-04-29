79851cefc0d5292fcb7b4458396537747d16c207
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-28 20:33:58 -0700

    added helper.js for date formating

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,43 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
+const { User, Comment, Post } = require('../models');
+
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User }, { model: Comment }],
+    });
+    const posts = postData.map(post => post.get({plain:true}))
+    res.render('homepage', {posts});
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+router.get("/createpost", async (req, res) => {
+  res.render("createpost");
+});
+
+router.get("/dashboard", async (req, res) => {
+  res.render("dashboard");
+});
+
+router.get("/editpost", async (req, res) => {
+  res.render("editpost");
+});
+
+router.get("/login", async (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", async (req, res) => {
+  res.render("signup");
+});
+
+router.get("/viewpost", async (req, res) => {
+  res.render("viewpost");
+});
+
+module.exports = router; 
