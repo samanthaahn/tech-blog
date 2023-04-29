4eea647bf9494db39908dfe0a9baf1f4eb25c477
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-28 20:38:49 -0700

    added a put route to edit a comment

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -13,9 +13,25 @@ try {
     });
     res.status(200).json(newComment);
 } catch (error) {
-    console.error('Error with this post:', error);
+    console.error('Error with this comment:', error);
     res.status(500).json({ message: 'Internal Server Error', error: error });
 }
 });
+
+// This is to edit a comment 
+router.put('/:id', async (req, res) => {
+    try {
+        await Comment.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        })
+        const updatedComment = await Comment.findByPk(req.params.id);
+        res.status(200).json(updatedComment);
+    } catch (error) {
+        console.error('Error with this comment:', error);
+        res.status(500).json({ message: 'Internal Server Error', error: error });
+    }
+})
 
 module.exports = router;
